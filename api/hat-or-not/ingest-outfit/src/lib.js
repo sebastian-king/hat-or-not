0bff1c4e69c707a69b7b935425d30da701b6306b
Sebastian King <sebastian.king@btinternet.com>
2022-09-25 00:24:50 +0200

    make api

--- api/hat-or-not/ingest-outfit/src/lib.js
+++ api/hat-or-not/ingest-outfit/src/lib.js
@@ -1,7 +1,7 @@
 import { log } from "@suborbital/runnable";
 
 export const run = (input) => {
-  let message = `Hello, ` + input;
+  let message = "Hello, " + input;
 
   log.info(message);
 
--- artificial-intelligence/color-detection/color-detection.py
+++ artificial-intelligence/color-detection/color-detection.py
@@ -3,9 +3,13 @@ import pandas as pd
 import cv2
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
-def histogram_equalization(image):
-    img_y_cr_cb = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+from fastapi import FastAPI, Request
+import uvicorn
+
+def histogram_equalization(img):
+    img_y_cr_cb = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
     y, cr, cb = cv2.split(img_y_cr_cb)
 
     # Applying equalize Hist operation on Y channel.
@@ -14,8 +18,8 @@ def histogram_equalization(image):
     img_y_cr_cb_eq = cv2.merge((y_eq, cr, cb))
     img_rgb_eq = cv2.cvtColor(img_y_cr_cb_eq, cv2.COLOR_YCR_CB2BGR)
     return img_rgb_eq
-    
-def getColorName(R,G,B):
+
+def getColorName(R,G,B,csv):
     minimum = 10000
     for i in range(len(csv)):
         d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
@@ -23,32 +27,52 @@ def getColorName(R,G,B):
             minimum = d
             cname = csv.loc[i,"color_name"]
             chex = csv.loc[i,"hex"]
-            
+
     return cname, chex
 
+def colorDetection(img):
+	if args['live']:
+            img = histogram_equalization(img)
+
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	plt.imshow(img)
+	plt.show()
+	#Reading csv file with pandas and giving names to each column
+	index=["color","color_name","hex","R","G","B"]
+	csv = pd.read_csv(os.path.join(os.getcwd(),'colors.csv'), names=index, header=None)
+
+	width = img.shape[1]
+	height = img.shape[0]
+	ypos = int(height/2)
+	xpos = int(width/2)
+	r,g,b = img[ypos,xpos]
+	color, hex = getColorName(r,g,b,csv) #+ ' R='+ str(r) + ' G='+ str(g) + ' B='+ str(b)
+	return color + " " + hex
+
 ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required=True, help="Image Path")
+ap.add_argument('-i', '--image', help="Image Path")
 ap.add_argument('-live', '--live', action='store_true')
-
+ap.add_argument('-serve', '--serve', action='store_true')
 args = vars(ap.parse_args())
-img_path = args['image']
-#Reading image with opencv
-img = cv2.imread(img_path)
-if args['live']:
-    img = histogram_equalization(img)
-    
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.imshow(img)
-plt.show()
-#Reading csv file with pandas and giving names to each column
-index=["color","color_name","hex","R","G","B"]
-csv = pd.read_csv(os.path.join(os.getcwd(),'colors.csv'), names=index, header=None)
 
-width = img.shape[1]
-height = img.shape[0]
-ypos = int(height/2)
-xpos = int(width/2)
-r,g,b = img[ypos,xpos]
-color, hex = getColorName(r,g,b) #+ ' R='+ str(r) + ' G='+ str(g) + ' B='+ str(b)
-print(color, hex)
-    
+if args['image'] is not True and args['serve'] is not True:
+    ap.error("requires one of --image or --serve.")
+
+img_path = args['image']
+
+if args['serve']:
+	print('Starting API server on port 8081')
+	app = FastAPI()
+
+	@app.post("/ingest/clothing")
+	async def ingest_clothing_image(request: Request):
+		data: bytes = await request.body()
+		bytes_as_np_array = np.frombuffer(data, dtype=np.uint8)
+		img = cv2.imdecode(bytes_as_np_array, cv2.IMREAD_ANYCOLOR)
+		return colorDetection(img)
+
+	uvicorn.run(app, host="0.0.0.0", port=8081)
+else:
+	#Reading image with opencv
+	img = cv2.imread(img_path)
+	colorDetection(img);
